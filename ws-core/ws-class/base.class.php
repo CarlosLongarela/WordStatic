a09d3f77380fbea649a19ad172f556381a35c7b7
Carlos Longarela <carlos@longarela.eu>
2014-07-03 18:05:28 +0200

    get and set title and description, sorry but now my daily work calls me, more tomorrow or maybe next week ;)

--- ws-core/ws-class/base.class.php
+++ ws-core/ws-class/base.class.php
@@ -11,11 +11,13 @@
  */
 
 class base {
-	use core;
+	use core, json;
 
 //***********************************************************//
 //**********************  VARS  ****************************//
 //*********************************************************//
+	private $ws_title 			= _('My New WS Site');
+	private $ws_description 	= _('My Fantastic WS Site, secure and very fast');
 
 //***************************************************************//
 //**********************  CONSTANTS  ***************************//
@@ -24,7 +26,23 @@ class base {
 //*************************************************************************//
 //****************************  PUBLIC METHODS  **************************//
 //***********************************************************************//
+	public ws_get_title() {
+		return $this->ws_title;
+	}//end ws_get_title
 
+	public ws_get_description() {
+		return $this->ws_description;
+	}//end ws_get_description
+
+	public ws_set_title($title) {
+		//@TODO: Checks about title
+		$this->ws_title=$title;
+	}//end ws_set_title
+
+	public ws_set_description($description) {
+		//@TODO: Checks about description
+		$this->ws_description=$description;
+	}//end ws_set_description
 //****************************************************************************//
 //****************************  PROTECTED METHODS  **************************//
 //**************************************************************************//
